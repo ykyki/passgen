1225acb39dc14f62b9efc356c7ca0072129d0880
ykyki <bsyki@pm.me>
2023-08-05 02:37:19 +0900

    define -v/--version option

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 var (
-	toolVersion = "0.0.1"
+	toolVersion = "0.1.0"
 )
 
 func main() {
-	fmt.Printf("passgen version %s\n", toolVersion)
+	var (
+		printVersion bool
+	)
+
+	flag.BoolVar(&printVersion, "version", false, "print this tool version")
+	flag.BoolVar(&printVersion, "v", false, "print this tool version (shorthand)")
+	flag.Parse()
+
+	if printVersion {
+		fmt.Printf("passgen version %s\n", toolVersion)
+		return
+	}
+
+	fmt.Println("passgen (TODO)")
 }

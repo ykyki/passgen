50dd9dd69cf66113604876d69d751232acb74f78
ykyki <bsyki@pm.me>
2023-08-05 16:31:24 +0900

    define cli struct to set output writer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,32 +12,52 @@ var (
 )
 
 func main() {
+	cli := &cli{outStream: os.Stdout, errStream: os.Stderr}
+	os.Exit(cli.run(os.Args))
+}
+
+type cli struct {
+	outStream, errStream io.Writer
+}
+
+func (c *cli) run(args []string) int {
+	f := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	f.SetOutput(c.errStream)
+
 	var (
 		printVersion bool
 		length       int
 	)
 
-	flag.BoolVar(&printVersion, "version", false, "print passgen version")
-	flag.BoolVar(&printVersion, "v", false, "print passgen version (shorthand)")
-	flag.IntVar(&length, "length", 16, "password length")
-	flag.IntVar(&length, "l", 16, "password length")
-	flag.Parse()
+	f.BoolVar(&printVersion, "version", false, "print passgen version")
+	f.BoolVar(&printVersion, "v", false, "print passgen version (shorthand)")
+	f.IntVar(&length, "length", 16, "password length")
+	f.IntVar(&length, "l", 16, "password length")
+
+	err := f.Parse(args[1:])
+	if err != nil {
+		// fmt.Fprintf(c.errStream, "failed to parse arguments\n%s\n", err)
+		fmt.Fprintln(c.errStream, "failed to parse arguments")
+		return 1
+	}
 
 	if printVersion {
-		fmt.Printf("passgen version %s\n", toolVersion)
-		os.Exit(0)
+		fmt.Fprintf(c.outStream, "passgen version %s\n", toolVersion)
+		return 0
 	}
 
 	if length <= 0 {
-		fmt.Println("error: length must be greater than 0")
-		os.Exit(1)
+		fmt.Fprintf(c.errStream, "length must be greater than 0\n")
+		return 0
 	}
 
 	password, err := generatePassword(length)
 	if err != nil {
-		fmt.Printf("error: %s\n", err)
-		os.Exit(1)
+		fmt.Fprintf(c.errStream, "failed to generate password\n%s\n", err)
+		return 1
 	}
 
-	fmt.Println(password)
+	fmt.Fprintf(c.outStream, "%s\n", password)
+
+	return 0
 }
